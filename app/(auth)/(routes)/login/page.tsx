d96090846e1a112b9f4d290d0aa6cfa8faa3c507
ANIMESH MANDAL <animeshmandal4400@gmail.com>
2023-10-14 22:19:42 +0530

    folder setup

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+    return ( 
+        <div className="">
+            This is login page
+        </div>
+     );
+}
+ 
+export default page;
---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+    return ( 
+        <div>
+            <p>This is register page</p>
+        </div>
+     );
+}
+ 
+export default page;
